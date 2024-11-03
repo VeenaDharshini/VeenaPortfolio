b05311b41351827c71376b6486f5bd9072c86603
Veena Dharshini <veenadharshini.bs@wakefit.co>
2024-11-03 18:29:48 +0530

    .

--- src/app/(about)/about/_components/tech_stack_carousel.tsx
+++ src/app/(about)/about/_components/tech_stack_carousel.tsx
@@ -8,24 +8,34 @@ import { TechStackCard } from "./tech_stack_card";
 
 export function TechStackCarousel() {
 	return (
-		<Carousel
-			opts={{ loop: true, align: "start" }}
-			plugins={[
-				Autoplay({
-					delay: 700,
-					stopOnInteraction: true,
-					stopOnFocusIn: true,
-				}),
-			]}
-			className="max-w-[210px] sm:max-w-[432px] md:max-w-[640px] lg:max-w-[882px] xl:max-w-[1084px] mx-auto">
-			<CarouselContent className="-ml-1">
-				{SKILLS.map((skill) => (
-					<TechStackCard
-						key={skill.id}
-						skill={skill}
-					/>
-				))}
-			</CarouselContent>
-		</Carousel>
+		<div className="mx-auto">
+			<div className="flex flex-col gap-1 mb-5 whitespace-nowrap w-fit">
+				<span className="w-24 h-1 bg-[#E41A6E] rounded-sm" />
+				<h1 className="text-3xl font-semibold text-purple-950">
+					Tech stacks I use
+				</h1>
+				<span className="w-24 h-1 bg-[#E41A6E] rounded-sm ml-auto" />
+			</div>
+
+			<Carousel
+				opts={{ loop: true, align: "start" }}
+				plugins={[
+					Autoplay({
+						delay: 700,
+						stopOnInteraction: true,
+						stopOnFocusIn: true,
+					}),
+				]}
+				className="max-w-[212px] sm:max-w-[432px] md:max-w-[640px] lg:max-w-[882px] xl:max-w-[1084px] mx-auto">
+				<CarouselContent className="-ml-1">
+					{SKILLS.map((skill) => (
+						<TechStackCard
+							key={skill.id}
+							skill={skill}
+						/>
+					))}
+				</CarouselContent>
+			</Carousel>
+		</div>
 	);
 }
--- src/app/(about)/about/page.tsx
+++ src/app/(about)/about/page.tsx
@@ -1,24 +1,40 @@
 import Image from "next/image";
+import { FaPlay } from "react-icons/fa";
 import { TechStackCarousel } from "./_components/tech_stack_carousel";
 
 export default function Work() {
 	return (
 		<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start mb-28 max-w-7xl mx-auto">
-			<Image
-				src="/assets/Skills.png"
-				alt="Skills"
-				width={500}
-				height={100}
-				className="mx-auto max-w-96 md:max-w-[640px]"
-			/>
-			<p>
-				Hey! I'm a passionate software engineer with two years of valuable
-				experience in web development, particularly in the e-commerce sector.
-				Although I'm new to freelancing, I'm eager to leverage my expertise
-				further. This space is where I share my thoughts, projects, and a
-				glimpse into my journey.
-			</p>
-			<h1>Tech stacks I use</h1>
+			<div className="flex flex-col md:flex-row gap-7 items-center md:max-w-[640px] mx-auto">
+				<Image
+					src="/assets/Veena.png"
+					alt="My Img"
+					width={250}
+					height={100}
+					className="mx-auto max-w-96 md:max-w-[640px] rounded-lg"
+				/>
+				<ul className="text-purple-950 font-semibold font-mono">
+					<li className="grid grid-flow-col mb-3">
+						<FaPlay className="m-2" />
+						<span>
+							Hey! I'm a passionate software engineer with two years of valuable
+							experience in web development, particularly in the e-commerce
+							sector.
+						</span>
+					</li>
+					<li className="grid grid-flow-col mb-3">
+						<FaPlay className="m-2" />
+						Although I'm new to freelancing, I'm eager to leverage my expertise
+						further.
+					</li>
+					<li className="grid grid-flow-col mb-3">
+						<FaPlay className="m-2" />
+						This space is where I share my thoughts, projects, and a glimpse
+						into my journey.
+					</li>
+				</ul>
+			</div>
+
 			<TechStackCarousel />
 		</main>
 	);
